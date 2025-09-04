4dbe835aebf6c17bf790234d9ca3e1c14e266b30
Math Scicl <scicluna.mathieu@hotmail.fr>
2025-09-04 12:12:35 +0200

    modif card

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -103,7 +103,7 @@ export default function Projects() {
         <Filters active={filter} onChange={setFilterUrl} filters={FILTERS} />
 
         {/* GRID */}
-        <div className="mt-12 grid gap-8 [grid-template-columns:repeat(auto-fit,minmax(260px,1fr))]">
+        <div className="mt-12 grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-stretch w-400">
           {filtered.map((p, i) => (
             <ProjectCard
               key={p.id}
